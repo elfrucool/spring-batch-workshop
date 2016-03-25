0c382b733e9133a18c1bfcc64feb857ee76aab44
gserrano <gserrano@switchfly.com>
2016-03-25 00:28:33 +0000

    list existing contacts

--- exercise-3/src/main/java/importaddresslist/ImportAddressController.java
+++ exercise-3/src/main/java/importaddresslist/ImportAddressController.java
@@ -6,12 +6,15 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class ImportAddressController {
@@ -21,9 +24,26 @@ public class ImportAddressController {
     @Autowired
     private Job importAddressListJob;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @RequestMapping(value = "/importaddress", method = RequestMethod.GET)
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("contacts", loadContacts());
         return "index";
+    }
+
+    private List<Contact> loadContacts() {
+        return jdbcTemplate.query(
+                "SELECT name,email,phone FROM contacts ORDER BY name",
+                (rs, rowNum) -> {
+                    Contact contact = new Contact();
+                    contact.setName(rs.getString("name"));
+                    contact.setEmail(rs.getString("email"));
+                    contact.setPhone(rs.getString("phone"));
+                    return contact;
+                }
+        );
     }
 
     @RequestMapping(value = "/importaddress", method = RequestMethod.POST)
